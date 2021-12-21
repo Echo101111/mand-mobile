9547b6694d6098246d28852457fd19b7d2cc8a49
Echo101111 <74162946+Echo101111@users.noreply.github.com>
2021-12-21 10:28:53 +0800

    Update demo0.vue

--- components/toast/demo/cases/demo0.vue
+++ components/toast/demo/cases/demo0.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="md-example-child md-example-child-toast md-example-child-toast-0">
-    <md-button @click="showTextToast">纯文字</md-button>
+    <md-button @click="showTextToast">啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊</md-button>
   </div>
 </template>
 
-<script>import {Toast, Button} from 'mand-mobile'
+<script>
+import {Toast, Button} from 'mand-mobile'
 
 export default {
   name: 'toast-demo',
@@ -21,4 +22,5 @@ export default {
     },
   },
 }
-</script>
+
+</script>
